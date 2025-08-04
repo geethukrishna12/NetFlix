1637ccbfc2c8da648084d3d83fd4af65cee2189e
geethu_un <geethukrishna108@gmail.com>
2025-08-04 21:38:16 +0530

    Designing of NetFlix started

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play </button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='description'>Lorem, ipsum dolor sit amet
+                 consectetur adipisicing elit. Harum amet nisi,
+                 reiciendis nulla labore, ratione ex repudiandae repellendus
+                  sint deserunt!
+            </h1>
+            
+        </div>
+        <div className="fade_bottom"></div>
+      
+    </div>
+  )
+}
+
+export default Banner
